9789273db3689e5665f5cbfd158f1076a36b6de3
Alex Rodriguez <arodtony89@gmail.com>
2020-09-03 13:33:04 -0400

    latest config and bash file

---
+++ mosaicism_bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#First read in the config file and store the values in a dictionary
+
+config=$1
+
+declare -A config_vals
+
+while IFS=$'\t' read -r -a myArray
+do
+	config_vals["${myArray[0]}"]="${myArray[1]}"
+done < $config
+
+#run mosaicism script
+
+./mosaicism $config
+
+# run python script to generate images, arguments are input file and output_directory
+
+python mosaicism_image_generator_script.py "${config_vals[OUTPUT_FILE_PATH]}" "${config_vals[IMAGE_DIRECTORY]}"

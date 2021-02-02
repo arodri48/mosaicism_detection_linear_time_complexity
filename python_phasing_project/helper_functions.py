0bb812b981da1b8feb3a5ddf30c1ab541a589ce2
Rodriguez <rodriguezah@nih.gov>
2021-02-02 17:01:23 -0500

    latest version of notebook and helper_functions

--- python_phasing_project/helper_functions.py
+++ python_phasing_project/helper_functions.py
@@ -6,9 +6,12 @@ def read_VCF(vcf_path, name_list):
     with open(vcf_path, 'r') as f:
         lines = [l for l in f if not l.startswith('##')]
     df = pd.read_csv(io.StringIO(''.join(lines)), sep='\t')
+    df = df[~df['REF'].str.contains(",")]
+    df = df[~df['ALT'].str.contains(",")]
+    df = df[~df['QUAL'].str.contains(".")]
     df['REF'] = df['REF'].astype('str')
     df['ALT'] = df['ALT'].astype('str')
-    df['QUAL'] = df['QUAL'].astype('int64')
+    df['QUAL'] = pd.to_numeric(df['QUAL'], errors='coerce')
     for elem in name_list:
         df[elem] = df[elem].astype('str')
     return df
@@ -23,8 +26,7 @@ def filter_VCF_by_chr_and_SNP(df, chr_number):
 def ped_file_reader(ped_file_path):
     with open(ped_file_path, 'r') as f:
         names = [l.strip().split('\t')[1] for l in f]
-    actual_names = [names[1], names[2]]
-    for i in range(len(names)):
-        if i != 1 and i != 2:
-            actual_names.append(names[i])
+    actual_names = [names[1], names[2], names[0]]
+    for i in range(3, len(names)):
+        actual_names.append(names[i])
     return actual_names

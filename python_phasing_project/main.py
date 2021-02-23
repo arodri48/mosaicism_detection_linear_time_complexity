74c203d6e35d4611fc54e13fea597923db0b9b06
Rodriguez <rodriguezah@nih.gov>
2021-02-23 12:40:57 -0500

    finished initial implementation of mosaicism detection

--- python_phasing_project/main.py
+++ python_phasing_project/main.py
@@ -1,5 +1,5 @@
-from child import Child
 import helper_functions
+import phasing_functions
 
 
 def main(config_file_path):
@@ -18,56 +18,22 @@ def main(config_file_path):
     # Step 3: obtain only SNPs
     SNP_df = helper_functions.SNP_filter(df)
 
-    # New step 4: Analyze only the proband and feed in finder by chromosome
-    chromosome_list = [i for i in range(1,23)]
-    chromosome_list.append("MT")
-    # TODO: make new constructor
-    proband = Child(names[2], names[0], names[1], SNP_df)
+    # Step 4: Generate results
+    mosaicism_outcome = [phasing_functions.runner(config_elem["PROBAND_NAME"], names[0], names[1], chr_name,
+                                                  int(config_elem["SAMPLE_SIZE"]), float(config_elem["T_THRES"]),
+                                                  SNP_df, float(config_elem["EDGE_DETECTION_WIDTH"])) for chr_name in
+                         range(1, 23)]
 
-    for chr_name in chromosome_list:
-        # TODO: Write this function
-        proband.runner(chr_name, config_elem["SAMPLE_SIZE"], config_elem["T_THRES"])
-
-    # New Step 5: Once each chromosome is ran, check output
-    if len(proband.results) == 0:
-        print("No mosaicism detected")
-    else:
-        print("mosaicism detected")
-        # TODO: Write output to file
-        for elem in proband.results:
-            print(elem)
-
-
-
-
-
-
-
-
-
-
-    # Step 4: Create list of children
-    children = [Child(names[i], names[0], names[1]) for i in range(2, len(names))]
-
-
-    # Step 5: Generate phasable SNP data for each child
-    for elem in children:
-        elem.phasable_snp_determiner(chr7_df)
-
-    # Step 6: Do sliding t-test for each child and see if possible mosaicism present
-    for elem in children:
-        elem.t_test_snps(10)
-
-    # Step 7: Check if a child is mosaic
-    mosaic_child_present = False
-    for elem in children:
-        if elem.index_diff_arr_start_of_mosaicism != 0:
-            mosaic_child_present = True
-            # detect region of mosaicism in the child
-
-    # if all clear, print out
-    if not mosaic_child_present:
-        print("No child is mosaic")
+    # Step 5: Write results to file
+    with open(config_elem["OUTPUT_FILE"], 'w') as output_file:
+        output_file.write(" ".join(["Mosaicism results for", config_elem["PROBAND_NAME"]]))
+        output_file.write("\n")
+        output_file.write("\t".join(["chr_number", "VCF_start", "VCF_end"]))
+        output_file.write("\n")
+        for i, elem in enumerate(mosaicism_outcome):
+            if elem is not None:
+                output_file.write("\t".join([str(i + 1), str(elem[0]), str(elem[1])]))
+                output_file.write("\n")
 
 
 main('phasing_config_file.txt')
--- python_phasing_project/phasing_functions.py
+++ python_phasing_project/phasing_functions.py
@@ -7,8 +7,8 @@ from math import floor
 
 def sandia_t_test_snps(maternal_rd, paternal_rd, samp_size=10000, t_thres=25):
     mom_minus_samp_size = maternal_rd.size - samp_size
+    # Check if sample size is greater read depth total
     if 0 > mom_minus_samp_size:
-        print("Sample size is larger than total number of data points")
         return None
     else:
         # Step 1: Allocate an array that will store the t
@@ -31,14 +31,11 @@ def sandia_t_test_snps(maternal_rd, paternal_rd, samp_size=10000, t_thres=25):
         if index_of_mosaicism != -1:
             # there is a t_value that exceeds the thresold, save the vcf position of the start
             index_start_of_mosaicism_vcf = index_of_mosaicism + samp_size - 1 if index_of_mosaicism != 0 else 0
-            #vcf_pos_start_of_mosaicism = vcf_pos[index_start_of_mosaicism_vcf]
             # figure out the end point of mosaicism
             index_of_end_of_mosaicism = next(
                 (i + index_of_mosaicism + 1 for i, elem in enumerate(t_values[index_of_mosaicism + 1:]) if
                  elem < t_thres), len(t_values) - 1)
             index_end_of_mosaicism_vcf = index_of_end_of_mosaicism + samp_size - 1
-            #index_end_of_mosaicism_vcf = index_of_end_of_mosaicism + samp_size - 1 if index_of_end_of_mosaicism != len(t_values) - 1 else -1
-            #vcf_pos_end_of_mosaicism = vcf_pos[index_end_of_mosaicism_vcf] 
             return [index_start_of_mosaicism_vcf, index_end_of_mosaicism_vcf]
         else:
             return None
@@ -76,25 +73,8 @@ def phasable_snp_determiner(chr_df, proband_name, father_name, mother_name):
                                     is_mom_hom_var and is_dad_hom_var)):
                         # save the position number and then the read depth for the child
                         pos_final.append(row['POS'])
-                        # case 1: Dad is hom var and mom is hom ref
-                        if is_dad_hom_var and is_mom_hom_ref:
-                            if child_info[0][0] == '1':
-                                dad_rd_final.append(child_rd_first)
-                                mom_rd_final.append(child_rd_second)
-                            else:
-                                dad_rd_final.append(child_rd_second)
-                                mom_rd_final.append(child_rd_first)
-                        # case 2: mom is hom var and dad is hom ref
-                        elif is_mom_hom_var and is_dad_hom_ref:
-                            if child_info[0][0] == '1':
-                                dad_rd_final.append(child_rd_second)
-                                mom_rd_final.append(child_rd_first)
-                            else:
-                                dad_rd_final.append(child_rd_first)
-                                mom_rd_final.append(child_rd_second)
-                        # case 3: Dad is a het
-                        elif is_dad_het:
-                            # if mom is hom ref
+                        # case 1: Dad is a het
+                        if is_dad_het:
                             if is_mom_hom_ref:
                                 if child_info[0][0] == '0':
                                     dad_rd_final.append(child_rd_second)
@@ -110,8 +90,8 @@ def phasable_snp_determiner(chr_df, proband_name, father_name, mother_name):
                                 else:
                                     dad_rd_final.append(child_rd_first)
                                     mom_rd_final.append(child_rd_second)
-                        # case 4: Mom is a het
-                        else:
+                        # case 2: Mom is a het
+                        elif is_mom_het:
                             # if dad is hom ref
                             if is_dad_hom_ref:
                                 if child_info[0][0] == '0':
@@ -128,17 +108,34 @@ def phasable_snp_determiner(chr_df, proband_name, father_name, mother_name):
                                 else:
                                     dad_rd_final.append(child_rd_second)
                                     mom_rd_final.append(child_rd_first)
+                        # case 3: dad is hom var and mom is hom ref
+                        elif is_dad_hom_var and is_mom_hom_ref:
+                            if child_info[0][0] == '1':
+                                dad_rd_final.append(child_rd_first)
+                                mom_rd_final.append(child_rd_second)
+                            else:
+                                dad_rd_final.append(child_rd_second)
+                                mom_rd_final.append(child_rd_first)
+                        # case 4: mom is hom var and dad is hom ref
+                        else:
+                            if child_info[0][0] == '1':
+                                dad_rd_final.append(child_rd_second)
+                                mom_rd_final.append(child_rd_first)
+                            else:
+                                dad_rd_final.append(child_rd_first)
+                                mom_rd_final.append(child_rd_second)
+
     return pos_final, np.array(mom_rd_final), np.array(dad_rd_final)
 
 
-def edge_detection(sample_size, estimated_start_index, estimated_end_index, paternal_rd_array, maternal_rd_array, vcf_pos):
+def edge_detection(sample_size, estimated_start_index, estimated_end_index, paternal_rd_array, maternal_rd_array,
+                   vcf_pos, edge_detection_width):
     estimated_interval_length = estimated_end_index - estimated_start_index
     width_of_average = floor(estimated_interval_length / 2)
     fourth_up = floor(estimated_interval_length / 4)
     diff_arr = paternal_rd_array - maternal_rd_array
-    print(diff_arr.size)
     height = diff_arr[estimated_start_index + fourth_up:estimated_start_index + fourth_up + width_of_average].mean()
-    filter_width_one_side = floor(0.25 * sample_size)
+    filter_width_one_side = floor(edge_detection_width / 2 * sample_size)
     forward_filter = np.zeros(2 * filter_width_one_side)
     forward_filter[filter_width_one_side:] = height
     backward_filter = np.zeros(2 * filter_width_one_side)
@@ -146,50 +143,56 @@ def edge_detection(sample_size, estimated_start_index, estimated_end_index, pate
 
     final_index = paternal_rd_array.size - 1
     is_mosaicism_to_the_end = estimated_end_index == final_index
-    print('Final index is '+ str(final_index)+' and estimated_end_index is '+str(estimated_end_index))
-
+    # case 1: whole chromosome is mosaic
     if not estimated_start_index and is_mosaicism_to_the_end:
-        # estimated chromosome is mosaic
-        return [vcf_pos[0], vcf_pos[final_index]]
+        return [vcf_pos[0], vcf_pos[final_index], 0, final_index, height]
+    # case 2: mosaicism starts and beginning and ends in the middle
     elif not (estimated_start_index or is_mosaicism_to_the_end):
         center_index = estimated_end_index - floor(0.5 * sample_size)
         filter_difference = [abs((diff_arr[
                                   center_index - filter_width_one_side + i: center_index + filter_width_one_side + i] - backward_filter).sum(
             dtype=float)) for i in range(sample_size)]
         min_val = min(filter_difference)
-        return [vcf_pos[0], vcf_pos[center_index + filter_difference.index(min_val)]]
+        return [vcf_pos[0], vcf_pos[center_index + filter_difference.index(min_val)], 0,
+                center_index + filter_difference.index(min_val), height]
+    # case 3: mosaicism starts in middle and ends at the telomere
     elif estimated_start_index and is_mosaicism_to_the_end:
         center_index = estimated_start_index - floor(0.5 * sample_size)
         filter_difference = [abs((diff_arr[
                                   center_index - filter_width_one_side + i: center_index + filter_width_one_side + i] - forward_filter).sum(
             dtype=float)) for i in range(sample_size)]
         min_val = min(filter_difference)
-        return [vcf_pos[center_index + filter_difference.index(min_val)], vcf_pos[final_index]]
+        return [vcf_pos[center_index + filter_difference.index(min_val)], vcf_pos[final_index],
+                center_index + filter_difference.index(min_val), final_index, height]
+    # case 4: mosaicism starts and ends somewhere in teh middle
     else:
         center_start_index = estimated_start_index - floor(0.5 * sample_size)
-        # TODO: debug
-        filter_start_difference = []
-        for i in range(sample_size):
-            curr_subarray = diff_arr[center_start_index - filter_width_one_side + i:center_start_index + filter_width_one_side + i]
-            print(curr_subarray.size)
-        
-        #filter_start_difference = [abs((diff_arr[center_start_index - filter_width_one_side + i: center_start_index + filter_width_one_side + i] - forward_filter).sum(dtype=float)) for i in range(sample_size)]
-        #min_start_val = min(filter_start_difference)
+        filter_start_difference = [abs((diff_arr[
+                                        center_start_index - filter_width_one_side + i: center_start_index + filter_width_one_side + i] - forward_filter).sum(
+            dtype=float)) for i in range(sample_size)]
+        min_start_val = min(filter_start_difference)
 
-        #center_end_index = estimated_end_index - floor(0.5 * sample_size)
-        
-        
-        
-        #filter_end_difference = [abs((diff_arr[center_end_index - filter_width_one_side + i:center_end_index + filter_width_one_side + i] - backward_filter).sum(dtype=float)) for i in range(sample_size)]
-        #min_end_val = min(filter_end_difference)
+        center_end_index = estimated_end_index - floor(0.5 * sample_size)
+        filter_end_difference = [abs((diff_arr[
+                                      center_end_index - filter_width_one_side + i:center_end_index + filter_width_one_side + i] - backward_filter).sum(
+            dtype=float)) for i in range(sample_size)]
+        min_end_val = min(filter_end_difference)
 
-        #return [vcf_pos[center_start_index + filter_start_difference.index(min_start_val)],vcf_pos[center_end_index + filter_end_difference.index(min_end_val)]]
+        return [vcf_pos[center_start_index + filter_start_difference.index(min_start_val)],
+                vcf_pos[center_end_index + filter_end_difference.index(min_end_val)],
+                center_start_index + filter_start_difference.index(min_start_val),
+                center_end_index + filter_end_difference.index(min_end_val), height]
 
 
-def runner(child, chr_name, sample_size, t_threshold, SNP_df):
+def runner(child_name, father_name, mother_name, chr_name, sample_size, t_threshold, SNP_df, edge_detection_width):
     # step 1: filter the SNP df by chromosome name
     chr_snp_df = helper_functions.chromosome_filter(SNP_df, chr_name)
-    # step 2: do phasing and return results
-    vcf_pos, maternal_rd, paternal_rd = phasable_snp_determiner(chr_snp_df, child.name, child.father_name, child.mother_name)
-    mosaicism_initial_survey_results = sandia_t_test_snps(maternal_rd, paternal_rd, samp_size=sample_size, t_thres=t_threshold)
-    return edge_detection(sample_size, mosaicism_initial_survey_results[0], mosaicism_initial_survey_results[1], paternal_rd, maternal_rd, vcf_pos) if mosaicism_initial_survey_results is not None else None
+    # step 2: do phasing
+    vcf_pos, maternal_rd, paternal_rd = phasable_snp_determiner(chr_snp_df, child_name, father_name, mother_name)
+    # step 3: determine if mosaicism is present
+    mosaicism_initial_survey_results = sandia_t_test_snps(maternal_rd, paternal_rd, samp_size=sample_size,
+                                                          t_thres=t_threshold)
+    # step 4: if it is, accurately determine edges
+    return edge_detection(sample_size, mosaicism_initial_survey_results[0], mosaicism_initial_survey_results[1],
+                          paternal_rd, maternal_rd, vcf_pos,
+                          edge_detection_width) if mosaicism_initial_survey_results is not None else None
